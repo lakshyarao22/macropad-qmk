0fd24f6d906e07625458411f855f0491b8ddcf27
Lakshya Rao <lakshyarao22@gmail.com>
2026-08-20 00:29:23 +0530

    Replace KC_NO with function keys in keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [2] = LAYOUT(
-        KC_NO, KC_NO, KC_NO,
+        KC_F22, KC_F23, KC_F24,
         LSG(KC_S), LGUI(KC_L), PASS,
         KC_NO, KC_NO, KC_NO
     ),
