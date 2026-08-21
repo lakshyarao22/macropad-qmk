16799942f2bb44a09ba41ff40bb896c372ae1f96
Lakshya Rao <lakshyarao22@gmail.com>
2026-08-22 00:19:33 +0530

    Use RP2040 hardware PWM for audio

--- halconf.h
+++ halconf.h
@@ -1,7 +1,5 @@
 #pragma once
 
 #define HAL_USE_PWM TRUE
-#define HAL_USE_GPT TRUE
-#define HAL_USE_PAL TRUE
 
 #include_next <halconf.h>
--- rules.mk
+++ rules.mk
@@ -7,5 +7,5 @@ BOOTLOADER = rp2040
 
 # Audio output on the spare GP28 pin
 AUDIO_ENABLE = yes
-AUDIO_DRIVER = pwm_software
+AUDIO_DRIVER = pwm_hardware
 MUSIC_ENABLE = yes
